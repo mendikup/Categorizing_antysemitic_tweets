cf78d76c44152a48c765fe1e0afa1b6a65b3d5c1
mendi kuperman <mendikuperman@gmail.com>
2025-08-05 15:00:14 +0300

    implement the data cleaner

--- src/dal/dal.py
+++ src/dal/dal.py
@@ -6,6 +6,9 @@ class Dal:
     def __init__(self):
         self._rwa_data = pd.read_csv("data/tweets_dataset.csv")
         self.json_path = "results/results.json"
+        self.cleaned_csv_path = "results/cleaned_dataset_tweets.csv"
+
+
 
 
     def load_data(self):
@@ -15,3 +18,6 @@ class Dal:
     def dump_json(self, data):
         with open(self.json_path ,"w" ,encoding="utf-8") as file:
             json.dump(data,file, indent= 4)
+
+    def dump_cleaned_csv(self, df):
+        df.to_csv(self.cleaned_csv_path, index=False, encoding="utf-8")
